3918346ec6964293d35e747ac9c93be5fd27b94f
unknown <jsberlanga@gmail.com>
2019-06-07 11:29:41 +0200

    useContext

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 
 import Form from "./components/Form/Form.js";
 import Counter from "./components/Counter";
 import Toggle from "./components/Toggle";
 import Ref from "./components/Ref";
 
+export const UserContext = createContext();
+
 const App = () => {
+  const [user, setUser] = useState(false);
+
+  const logUser = () => {
+    setUser(user === true ? false : true);
+  };
+
   return (
-    <div className="main-wrapper">
-      <h1>React Hooks</h1>
-      <Form />
-      <Toggle />
-      <Counter />
-      <Ref />
-    </div>
+    <UserContext.Provider value={{ user, logUser }}>
+      <div className="main-wrapper">
+        <h1>React Hooks</h1>
+        <Form />
+        <Toggle />
+        <Counter />
+        <Ref />
+      </div>
+    </UserContext.Provider>
   );
 };
 
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,20 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { UserContext } from "../App.js";
 
 const Toggle = () => {
   const [isToggled, setIsToggled] = useState(false);
 
+  const userInfo = useContext(UserContext);
+
   return (
     <div>
       <h3>Toggle Component</h3>
-      <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
-      {isToggled ? (
-        <span style={{ fontSize: "2rem" }} role="img" aria-label="smiley">
-          😀
-        </span>
+
+      {userInfo.user ? (
+        <>
+          <button onClick={() => userInfo.logUser()}>Logout</button>
+          <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
+          {isToggled ? (
+            <span style={{ fontSize: "2rem" }} role="img" aria-label="smiley">
+              😀
+            </span>
+          ) : (
+            <span style={{ fontSize: "2rem" }} role="img" aria-label="thinker">
+              🙄
+            </span>
+          )}
+        </>
       ) : (
-        <span style={{ fontSize: "2rem" }} role="img" aria-label="thinker">
-          🙄
-        </span>
+        <>
+          <button onClick={() => userInfo.logUser()}>Login</button>
+          <p>You must be logged in to use the toggle</p>
+        </>
       )}
     </div>
   );
